dede7da172557ddea6c83fd629248286755f4255
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-09 22:44:51 +0000

    first commit

---
+++ count.py
@@ -0,0 +1,7 @@
+StartNumber = int(input("Enter 0 or 1: "))
+EndNumber = int(input("Enter number you want to reach: "))
+
+while StartNumber < EndNumber:
+
+    StartNumber += 1
+    print(StartNumber)
