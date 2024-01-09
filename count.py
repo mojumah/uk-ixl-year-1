b048ae3a436f1aee63b6b6a95eac6013c30e3ddb
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-09 23:35:10 +0000

    adding validation

--- count.py
+++ count.py
@@ -1,7 +1,13 @@
 StartNumber = int(input("Enter 0 or 1: "))
-EndNumber = int(input("Enter number you want to reach: "))
 
-while StartNumber < EndNumber:
+if ((StartNumber != 0) and (StartNumber != 1)):
+    print('Enter 0 or 1')
 
-    StartNumber += 1
-    print(StartNumber)
+else:
+
+    EndNumber = int(input("Enter number you want to reach: "))
+
+    while StartNumber < EndNumber:
+
+        StartNumber += 1
+        print(StartNumber)
