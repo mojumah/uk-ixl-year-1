9e4407358ee6fa1f99f544f42318c3dbc587804b
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-10 21:42:19 +0000

    quiz the user, what is the next number?

---
+++ next-number.py
@@ -0,0 +1,10 @@
+series = [1, 2, 3]
+last_number = series[-1]
+print("Enter the number that comes after")
+print (last_number)
+answer = int(input("Enter your answer:"))
+
+if (answer == 4):
+    print('correct')
+else:
+    print('wrong')
