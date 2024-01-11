7723f486e28a1b2550f1e47e3638f099575f5da6
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-11 23:45:40 +0000

    allowing quiz master to enter a list of any length with any numbers

--- next-number.py
+++ next-number.py
@@ -1,10 +1,20 @@
-series = [1, 2, 3]
+series = []
+
+n = int(input("Enter number of elements : "))
+
+for i in range (n):
+    element = int(input("Enter number:"))
+    series.append(element)
+
+print(series)
+
 last_number = series[-1]
 print("Enter the number that comes after")
 print (last_number)
 answer = int(input("Enter your answer:"))
+validate = answer - last_number
 
-if (answer == 4):
+if (validate == 1):
     print('correct')
 else:
     print('wrong')
