6237e5d6def703fad5dff2d8ca6085340829df22
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-18 23:09:22 +0000

    code to train your child how to compare

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = 5
+y = 9
+print("Is " + str(x) + " larger than " + str(y) + " ? ")
+print("Enter y for yes, and n for no ")
+answer = input("Enter your answer: ")
+if answer == 'n':
+    print("bravo")
+else:
+    print("try again")
