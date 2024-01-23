2f0eb59613171f5c8378b33ad1ffa1b47d0f34ae
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-23 23:35:30 +0000

    a function to calculate your days of experince

---
+++ days-of-experince.py
@@ -0,0 +1,10 @@
+import time
+import math
+import datetime
+from datetime import date
+today = date.today()
+start = datetime.date(2009, 1, 8)
+days_of_expreince = today - start
+print("I have")
+print(days_of_expreince.days)
+print("days of experince")
