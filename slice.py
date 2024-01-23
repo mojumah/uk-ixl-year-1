5271e1d8716456f0241e8f99add21df825f26d38
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-01-23 16:35:24 +0000

    adding nick name

--- slice.py
+++ slice.py
@@ -1,3 +1,8 @@
 name = input('Enter your father\'s name: ')
-sliced_name = name[2:8]
-print(sliced_name.capitalize())
+selector = int(input('Enter 1 to get nick name 1 or any other number to get nick name 2: '))
+if selector == 1:
+    sliced_name = name[2:8]
+    print(sliced_name.capitalize())
+else:
+    sliced_name_two = name[0:2]
+    print(sliced_name_two)
