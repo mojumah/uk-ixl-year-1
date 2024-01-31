7abd6188d25e44cffe7b91fbb00887cf5fce03b5
mojumah <mohammad_jumah@outlook.com>
2024-01-31 20:52:34 +0000

    containarizing app

---
+++ containers/days-of-experince.py
@@ -0,0 +1,10 @@
+import time
+import math
+import datetime
+from datetime import date
+today = date.today()
+start = datetime.date(2019, 1, 6)
+days_of_expreince = today - start
+print("I have")
+print(days_of_expreince.days)
+print("days of experince")
--- containers/efficient-counting.py
+++
@@ -1,5 +0,0 @@
-x = 0
-y = int(input("Enter the number you want to count to: "))
-while x <= y:
-    print(x)
-    x = x + 1
