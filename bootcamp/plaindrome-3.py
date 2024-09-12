92145aca2dcf9e9b9bac7b2bad69c7e400703a38
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-09-12 21:28:15 +0100

    script that categorizes plaindrome words and none plaindrome words

--- bootcamp/plaindrome-3.py
+++ bootcamp/plaindrome-3.py
@@ -1,8 +1,19 @@
+list = []
+plaindrome = []
+normal = []
+
 x = 0
-y = int(input("Enter the length of the list"))
+y = int(input("Enter the number of words you want to test if they are plaindromes or not: "))
 while x < y:
     x = x + 1
-    word = input("Enter the first word: ")
-    mylist = []
-    mylist.append(word)
-    print(mylist)
+    word = input("Enter the word number " + str(x) + "")
+    word_reversed = word[::-1]
+    if word == word_reversed:
+        plaindrome.append(word_reversed)
+    else:
+        normal.append(word)
+
+print("the list of plaindrome words are " )
+print(plaindrome)
+print("the list of normal words are ")
+print(normal)
