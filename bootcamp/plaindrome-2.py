0024227d2da15d26020d7d78cde800d56a1c37c6
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-09-12 21:27:00 +0100

    removing parts

--- bootcamp/plaindrome-2.py
+++
@@ -1,8 +0,0 @@
-y = int(input("Enter the length of the list"))
-for i in y:
-    print(y)
-
-    word = input("Enter the first word: ")
-    mylist = []
-    mylist.append(word)
-    print(mylist)
--- bootcamp/plaindrome.py
+++
@@ -1,7 +0,0 @@
-word = input('Enter the word you want to test: ')
-print(word)
-reverse = word[::-1]
-if word == reverse:
-    print('This word is a plaindrome')
-else:
-    print('This word is not a plaindrome')
