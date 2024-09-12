8e4be9a776f1573ad5c15d1a6edf9e42e9d952e3
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-09-12 15:43:51 +0100

    adding plaindrome code

---
+++ bootcamp/plaindrome-2.py
@@ -0,0 +1,8 @@
+y = int(input("Enter the length of the list"))
+for i in y:
+    print(y)
+
+    word = input("Enter the first word: ")
+    mylist = []
+    mylist.append(word)
+    print(mylist)
---
+++ bootcamp/plaindrome-3.py
@@ -0,0 +1,8 @@
+x = 0
+y = int(input("Enter the length of the list"))
+while x < y:
+    x = x + 1
+    word = input("Enter the first word: ")
+    mylist = []
+    mylist.append(word)
+    print(mylist)
